9245abfde4948a9c72b10e12a4a99405ce12da77
milagrospalma <mnpalma.2@gmail.com>
2018-03-21 05:33:49 -0500

    Añade estructura del componente purchaseDetails

--- src/lib/components/PurchaseDetails.js
+++ src/lib/components/PurchaseDetails.js
@@ -4,7 +4,21 @@ import '../styles/PurchaseDetails.css';
 const PurchaseDetails = () => (
   <div className="container">
     <div className="row details">
-     
+      <div className="col-4 code-box">
+        <p>Código de pago</p>
+        <h4>9125682</h4>
+      </div>
+      <div className="col-4 amount-box">
+        <p>Total</p>
+        <h5>S/140.00</h5>
+      </div>
+      <div className="col-4 expiry-box">
+        <span>
+          {/* <i className="fas fa-clock" /> */}
+        </span>
+        <p>Tu orden expirará</p>
+        <p>23 Feb - 01:12 p.m.</p>
+      </div>
     </div>
   </div>
 
